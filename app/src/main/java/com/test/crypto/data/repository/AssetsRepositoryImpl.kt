3423fabd5fed1c00e1056a7d60953cd033712b1b
Alex Patskevich <aliaksandr.patskevich@gmail.com>
2025-03-28 19:13:33 -0700

    Add CoroutineDispatcher to repository

--- app/src/main/java/com/test/crypto/data/repository/AssetsRepositoryImpl.kt
+++ app/src/main/java/com/test/crypto/data/repository/AssetsRepositoryImpl.kt
@@ -3,21 +3,27 @@ package com.test.Cryptotest.data.repository
 import com.test.Cryptotest.data.cache.AssetsCache
 import com.test.Cryptotest.data.model.Asset
 import com.test.crypto.data.api.CryptoApiService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AssetsRepositoryImpl @Inject constructor(
     private val cryptoApiService: CryptoApiService,
-    private val assetsCache: AssetsCache
+    private val assetsCache: AssetsCache,
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) : AssetsRepository {
 
     override suspend fun getAssets(): List<Asset> {
         val useCache = true
 
-        assetsCache.assetsCached?.let { if (useCache) return it }
+        return withContext(defaultDispatcher) {
+            assetsCache.assetsCached?.let { if (useCache) return@withContext it }
 
-        val assets = cryptoApiService.getAssets().data
-        assetsCache.assetsCached = assets
+            val assets = cryptoApiService.getAssets().data
+            assetsCache.assetsCached = assets
 
-        return assets
+            return@withContext assets
+        }
     }
 }
